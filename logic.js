d390ddf20a93c0600a527e0780f07d32c0e96502
Angel ortiz III <brainjello14@gmail.com>
2026-02-15 18:26:30 -0500

    Flex Boxes and Toggle Button
    I add flexboxes to better portion out the structure, photos and toggle dark and light button.

---
+++ logic.js
@@ -0,0 +1,12 @@
+let Connection_Count_El = document.getElementById("Connection_Count")
+let Connection_Count = 0;
+function AddConnnection(){
+    Connection_Count +=1
+    Connection_Count_El.textContent = Connection_Count
+}
+
+const toggleBtn = document.getElementById('toggle-mode');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
